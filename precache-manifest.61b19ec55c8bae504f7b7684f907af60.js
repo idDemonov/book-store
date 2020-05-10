bc2bf332fb610697c856d81420551f3dcb64edd6
idDemonov <rustamdemonov@gmail.com>
2020-05-10 21:05:59 +0300

    Updates

---
+++ precache-manifest.61b19ec55c8bae504f7b7684f907af60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "265abd52b40f17f57487e2662c57f4a1",
+    "url": "/book-store/index.html"
+  },
+  {
+    "revision": "1f972750b4a264d37364",
+    "url": "/book-store/static/js/2.7ef383e8.chunk.js"
+  },
+  {
+    "revision": "265068e7f3bc904803e029152d67e9a1",
+    "url": "/book-store/static/js/2.7ef383e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d10f38f93068b9337e3",
+    "url": "/book-store/static/js/main.263c9dbb.chunk.js"
+  },
+  {
+    "revision": "fe9889e4d1d393eb646b",
+    "url": "/book-store/static/js/runtime-main.566ef94a.js"
+  }
+]);
