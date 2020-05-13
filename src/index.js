eb68f2bd972cf2ba57b4dd30531f760814a1f652
idDemonov <rustamdemonov@gmail.com>
2020-05-13 16:13:01 +0300

    readme

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { getBook } from "./redux/actions/action-creators";
 
 render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>,
