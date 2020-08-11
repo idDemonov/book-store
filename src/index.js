6e923a774a15cc9229aef56586b32aef57386365
Demonov <rustamdemonov@gmail.com>
2020-08-11 18:29:40 +0300

    Google Analytics test

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ import { store } from "./redux/store";
 // Components
 import { App } from "./components/app";
 import { getBook } from "./redux/actions/action-creators";
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+    gtmId: 'GTM-N7F55M2',
+}
+
+TagManager.initialize(tagManagerArgs)
 
 render(
   <Provider store={store}>
